e09186cc5326612d0b98f0e0c934352de2c792f6
Brian Kilpatrick <thebriankilpatrick@gmail.com>
2020-01-16 18:33:06 -0500

    Added writeFile functionality, but needs debugging

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
 
+const writeFileAsync = util.promisify(fs.writeFile);
+
 getGithub()
 
 async function getGithub() {
@@ -21,13 +23,109 @@ async function getGithub() {
         const githubResult = await axios.get(
             `https://api.github.com/users/${github.user}`
         );
-        console.log(githubResult);
-        
+        console.log(githubResult.data);
+        // return githubResult;
     }
     catch (err) {
         console.log(err);
     }
 }
 
-// getGithub function does not work as expected.  The github account that gets passed in, is showing as null.
-// Thus, no account will pull
+function generateHTML() {
+    return `
+    <!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+        <link rel="stylesheet" href="assets/style.css">
+    </head>
+    <body>
+        <nav>
+            <div class="nav-wrapper indigo lighten-2">
+              <a href="#" class="brand-logo center">${githubResult.data.name}</a>
+            </div>
+        </nav>
+        <div class="container">
+            <div class="row">
+                <div class="col s12 m12 l12">
+                    <div class="card-panel indigo lighten-2 center-align">
+                        <a>
+                            <img class="circle" src="">
+                        </a>
+                        <h3 class="cardTitle">Hello there!</h3>
+                        <h4 class="cardTitle">My name is ${githubResult.data.name}</h4>
+                        <span class="white-text">
+                            ${githubResult.data.location}
+                        </span>
+                        <br>
+                        <span class="white-text">
+                            Github:${githubResult.data.html_url}  Blog: ${githubResult.data.blog}
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col s12 m12 l12">
+                    <div class="card-panel indigo lighten-2 center-align">
+                        <span class="white-text">
+                            ${githubResult.data.bio}
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col s6 m6 l6">
+                    <div class="card-panel indigo lighten-2 center-align">
+                        <h4 class="cardTitle">Public Repositories</h4>
+                        <span class="white-text">
+                        ${githubResult.data.public_repos}
+                        </span>
+                    </div>
+                </div>
+                <div class="col s6 m6 l6">
+                    <div class="card-panel indigo lighten-2 center-align">
+                        <h4 class="cardTitle">Followers</h4>
+                        <span class="white-text">
+                            ${githubResult.data.followers}
+                        </span>
+                    </div>
+                </div>
+                <div class="col s6 m6 l6">
+                    <div class="card-panel indigo lighten-2 center-align">
+                        <h4 class="cardTitle">GitHub Stars</h4>
+                        <span class="white-text">
+                            
+                        </span>
+                    </div>
+                </div>
+                <div class="col s6 m6 l6">
+                    <div class="card-panel indigo lighten-2 center-align">
+                        <h4 class="cardTitle">Following</h4>
+                        <span class="white-text">
+                            ${githubResult.data.following}
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>`;
+}
+
+// getGithub()
+//   .then(function(githubResult) {
+//     const html = generateHTML(githubResult);
+
+//     return writeFileAsync("index.html", html);
+//   })
+//   .then(function() {
+//     console.log("Successfully wrote to index.html");
+//   })
+//   .catch(function(err) {
+//     console.log(err);
+// });
+
